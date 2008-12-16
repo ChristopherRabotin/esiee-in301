7ace0fa97b929e96d73b7a582ee57c144635efe0
christopher.rabotin <christopher.rabotin@f890382c-c2b1-11dd-aab6-efc349ba664b>
2008-12-16 02:40:20 +0000

    Ajout doc pour communication.h Split terminal.c -> terminal.c + terminal.h

---
+++ esiee-in301/terminal.h
@@ -0,0 +1,10 @@
+#ifndef TERMINAL_H_
+#define TERMINAL_H_
+// includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// prototypes
+int main(int, char **);
+#endif /*TERMINAL_H_*/
