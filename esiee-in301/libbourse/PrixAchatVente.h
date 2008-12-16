102d55b305e2f47b395fa40fdaa0df7db9d22b6b
christopher.rabotin <christopher.rabotin@f890382c-c2b1-11dd-aab6-efc349ba664b>
2008-12-16 22:29:38 +0000

    Ajout d'un répertoire de communication (libcomm): contiendra la communication entre thread de même que la fonction de logger et la gestion des messages

--- esiee-in301/libbourse/PrixAchatVente.h
+++ esiee-in301/libbourse/PrixAchatVente.h
@@ -1,2 +1,6 @@
+/**
+ * Code fourni dans libbourse: fonction de calcul de prix d'achat et de vente
+ */
+
 double PrixAchat(double VInit, int Stock, int Qte);
 double PrixVente(double VInit, int Stock, int Qte);
--- esiee-in301/terminal.c
+++ esiee-in301/terminal.c
@@ -1,6 +1,6 @@
 #include "terminal.h"
 
 int main(int argc, char ** argv) {
-
+	printf("terminal.h");
 	return 0;
 }
