125b22a57d2a1eb4d9ae5898affea046decd02f8
Chris <ChristopherRabotin@users.noreply.github.com>
2008-12-16 22:41:55 +0000

    Déplacement de message.* dans libcomm Correction de la doc de libbourse qui était particulièrement foireuse (mise en page à la javadoc, comme vu dans BEAUCOUP de projet C)

--- libbourse/PrixAchatVente.c
+++ libbourse/PrixAchatVente.c
@@ -1,12 +1,6 @@
 
 
-double PrixAchat(double VInit, int Stock, int Qte)
-  /* ou VInit est la valeur initial de l'action 
-     ou Stock est la quantite d'action offerte 
-     ou Qte est la qte d'action dont on veut connaitre le prix d'achat
-     retourne la valeur total d'achat de Qte actions dont le prix
-     initial etait VInit et le stock courant est de Stock */ 
-{
+double PrixAchat(double VInit, int Stock, int Qte){
   int i;
   double facteur =0.0;
 
@@ -21,13 +15,7 @@ double PrixAchat(double VInit, int Stock, int Qte)
 
 }
 
-double PrixVente(double VInit, int Stock, int Qte)
-  /* ou VInit est la valeur initial de l'action
-   ou Stock est la quantite d'action offerte
-   ou Qte est la qte d'action dont on veut connaitre le prix
-   retourne la valeur total de vente de Qte actions dont le prix
-   initial etait VInit et le stock courant est de Stock */ 
-{
+double PrixVente(double VInit, int Stock, int Qte){
   int i;
   double facteur =0.0;
 
--- libbourse/PrixAchatVente.h
+++ libbourse/PrixAchatVente.h
@@ -2,5 +2,27 @@
  * Code fourni dans libbourse: fonction de calcul de prix d'achat et de vente
  */
 
+/**
+ * PrixAchat: calcule le prix d'achat d'actions, suivant le stock disponible
+ * et la quantité voulue
+ * @param VInit valeur initiale de l'action
+ * @param Stock quantité totale d'actions disponible
+ * @param Qte quantité d'action voulue
+ * @return prix d'achat total pour la quantité d'actions voulue au cours actuel  
+ */
 double PrixAchat(double VInit, int Stock, int Qte);
+
+/**
+ * PrixVente: calcule le prix de vente des actions, suivant le stock disponible
+ * et la quantité à vendre
+ * @param VInit valeur initiale de l'action
+ * @param Stock quantité totale d'actions disponible
+ * @param Qte quantité d'action à vendre
+ * @return prix de vente total pour la quantité d'actions à vendre au cours actuel  
+ */
+/* ou VInit est la valeur initial de l'action
+ ou Stock est la quantite d'action offerte
+ ou Qte est la qte d'action dont on veut connaitre le prix
+ retourne la valeur total de vente de Qte actions dont le prix
+ initial etait VInit et le stock courant est de Stock */
 double PrixVente(double VInit, int Stock, int Qte);
