6454f5f2cf1f5178b4e30757c057424dcf69f88c
Chris <ChristopherRabotin@users.noreply.github.com>
2008-12-21 08:24:15 +0000

    Tentative d'ajout du struct pour les messages. Néanmoins celui-ci n'est pas encore utilisable.
    Des tests sont en cours dans TestMessage.c mais celui-ci ne compilant pas, il n'a pas été ajouté au SVN.

--- libcomm/message.c
+++ libcomm/message.c
@@ -1,5 +1,6 @@
 #include "message.h"
 
+
 int decoupe(char *message, char *emeteur, char *type, char *action,
 		char *valeur) {
 	int nb=sscanf(message, "|%[^|]|%[^|]|%[^|]|%[^|]|\n", emeteur, type,
@@ -22,5 +23,37 @@ char* message(char *emeteur, char *type, char *action, char * valeur) {
 
 	sprintf(mess, "|%s|%s|%s|%s|\n", emeteur, type, action, valeur);
 
+	return mess;
+}
+
+msg *create_msg(char *emetteur, char *type, char *action, char *valeur) {
+	msg *rtn;
+	if (strcmp(type, "Achat") != 0&& strcmp(type, "Vente") != 0&& strcmp(type,
+			"AccuseVente") != 0&& strcmp(type, "AccuseAchat") != 0) {
+		log_call("Un message de type invalide a été rencontré!", "create_msg",
+				"(\"emetteur=%s,type=%s,action=%s,valeur=%s\")", emetteur,
+				type, action, valeur);
+		return NULL;
+	}
+#ifdef DEBUG
+	log_call("Création d'un message", "create_msg",
+			"(\"emetteur=%s,type=%s,action=%s,valeur=%s\")", emetteur, type,
+			action, valeur);
+#endif
+	strcpy(rtn->action, action);
+	strcpy(rtn->type, type);
+	strcpy(rtn->emetteur, emetteur);
+	strcpy(rtn->valeur, valeur);
+	return rtn;
+}
+
+char *msg_to_str(msg *msg) {
+	int longueur = strlen(msg->emetteur) + strlen(msg->type)
+			+ strlen(msg->action)+ strlen(msg->valeur) + 1+ 5+ 1;
+	char *mess = calloc(longueur, sizeof(char));
+	decoupe(mess, msg->emetteur, msg->type, msg->action, msg->action);
+#ifdef DEBUG
+	log_call("decoupe un msg", "*msg_to_str", "\t renvoie [%s]", mess);
+#endif
 	return mess;
 }
--- libcomm/message.h
+++ libcomm/message.h
@@ -4,17 +4,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "./logger.h"
 
 /**
  * msg correspond aux données qui transitent sur le réseau interboursier. Ces données sont nommés
  * <i>messages</i> dans le sujet du projet.
  */
-typedef struct {
+typedef struct msg_struct {
 	char emetteur[32]; //  l’identiﬁant de l’émetteur de l’ordre ;
 	char type[11]; // – T...T est le type du message: ’Achat’ ’Vente’ ’AccuseVente’ ’AccuseAchat’
 	char action[20]; // t l’identiﬁcateur de l’action sur laquelle porte l’ordre ou l’accusé de réception
 	char valeur[5];
 } msg;
+
+/** 
+ * create_msg: Construction du message. Le message est alloué dans la fonction.
+ * @param *emeteur émeteur du message
+ * @param *type type du message
+ * @param *action identificateur de l'action
+ * @param *valeur valeur associée au message
+ * @return le message de type msg
+ */
+msg* create_msg(char *emeteur, char *type, char *action, char *valeur);
+
+/**
+ * msg_to_str: transforme le message msg en une chaine de charactère suivant le protocole utilisé
+ * Fait appel à découpe
+ */
+char* msg_to_str(msg *msg);
 
 /**
  * decoupe: Decoupe du message
