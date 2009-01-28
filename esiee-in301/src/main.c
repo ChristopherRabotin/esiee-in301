0cec0b47fbcbf7376d4283952812dd4598d4943d
christopher.rabotin <christopher.rabotin@f890382c-c2b1-11dd-aab6-efc349ba664b>
2009-01-28 15:02:47 +0000

    Déplacement du main.c et de casual_includes dans src/

--- esiee-in301/src/main.c
+++ esiee-in301/src/main.c
@@ -5,8 +5,8 @@ int main(void){
 	printf("Lancement du programme\n");
 	init_log("bourse.log");
 	server *execution; server *acquisition;
-	init_one_server(execution, 1200, 2, "execution");
-	init_one_server(acquisition, 1200, 2, "acquisition");
+	init_one_server(execution, 1200, 2, "execution",exec);
+	init_one_server(acquisition, 1200, 2, "acquisition",ack);
 	sleep(100);
 	printf("fin.\n")
 	return 0;
