f44f29847e6fbd8c7c12e8745f27edcdcdac09d8
Chris <ChristopherRabotin@users.noreply.github.com>
2009-01-28 15:42:47 +0000

    TODO: terminal créer une fonction lançant un serveur et un client

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int main(void){
 	init_one_server(&servers[0], 1200, 2, "execution",exec);
 	init_one_server(&servers[1], 1201, 2, "acquisition",ack);
 	init_one_server(&servers[2], 1202, 2, "interboursier",interb);
+	//TODO : terminal: il faut créer une fonction qui lance un serveur et un client (discrètement)
 	sleep(100);
 	printf("fin.\n");
 	return 0;
